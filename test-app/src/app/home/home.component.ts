263470369e7c035ad069f0fe8ec4e8e7d7ad0aae
SamienShaheed <samienshaheed@gmail.com>
2025-02-11 20:05:24 +0600

    testing with signals and event listeners

--- test-app/src/app/home/home.component.ts
+++ test-app/src/app/home/home.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { GreetingComponent } from '../component/greeting/greeting.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [GreetingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  homeMessage = signal("Parent to Child signal");
 
+  keyUpHandler(event: KeyboardEvent) {
+    console.log(`user pressed ${event.code}`);
+  }
 }
